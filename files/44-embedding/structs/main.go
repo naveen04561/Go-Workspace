f33c560854d8d7502740c2cf3a9d58d18c03a0e8
Naveen <72813093+naveen04561@users.noreply.github.com>
2023-09-19 23:29:43 +0530

    Add files via upload

---
+++ files/44-embedding/structs/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	fmt.Printf("%+v", jim)
+}
---
+++ files/53-manipulating/map/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	colors := make(map[int]string)
+
+	colors[10] = "#ffffff"
+
+	delete(colors, 10)
+
+	fmt.Println(colors)
+}
